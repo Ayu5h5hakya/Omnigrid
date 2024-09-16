d2a1ec9037db111db4d17914db9595932b2290a4
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2024-09-16 11:01:56 +0545

    make omni grid stateful

---
+++ lib/omni_grid.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OmniGrid extends StatefulWidget {
+  const OmniGrid({super.key});
+
+  @override
+  State<OmniGrid> createState() => _OmniGridState();
+}
+
+class _OmniGridState extends State<OmniGrid> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
